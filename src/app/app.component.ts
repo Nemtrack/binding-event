04d9d84b5156bfbf99f82c75ec44b70404b8e5b4
Nemtrack <balazsgyenes10@gmail.com>
2023-11-19 21:44:26 +0100

    Finish example for ngStyle and ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-app';
+  buttonClicks: number[] = [];
+  numberOfClicks = 0;
+
+  showDetails = false;
+  onDisplayDetails() {
+    this.showDetails = !this.showDetails;
+    this.numberOfClicks++;
+    this.buttonClicks.push(this.numberOfClicks);
+  }
+
+  blueIfMoreThan5() {
+    return this.numberOfClicks >= 5 ? 'blue' : '';
+  }
 }
